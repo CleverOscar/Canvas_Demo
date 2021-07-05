049b8d0c6313b029001b7e784eab18ba8f904710
Oscar Pacheco <Oscarpa9188@gmail.com>
2021-07-05 11:25:47 -0500

    Basic Actions

--- canvas.js
+++ canvas.js
@@ -2,3 +2,26 @@ var canvas = document.getElementById('canvas');
 
 var ctx = canvas.getContext('2d');
 
+
+// ctx.moveTo(0,0)
+// ctx.lineTo(200,100);
+// ctx.stroke();
+
+
+// ctx.beginPath();
+// ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+// ctx.str
+
+// ctx.font = "14px Arial";
+// ctx.fillText('Hi from Canvas', 40, 40);
+
+// ctx.font = "24px Arial";
+// ctx.strokeText("Hello from inside canvas", 30, 100);
+
+// var grd = ctx.createLinearGradient(75, 50, 5, 90, 60, 100);
+
+// grd.addColorStop(0, "red");
+// grd.addColorStop(1, "white");
+
+// ctx.fillStyle = grd;
+// ctx.fillRect(10,10,150,80);
